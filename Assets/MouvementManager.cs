bfd71fd68853e152fe3913963138c3ca2f0a614e
Antislash <gataf91@hotmail.fr>
2018-05-26 12:45:32 +0200

    First commit

---
+++ Assets/MouvementManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MouvementManager : MonoBehaviour {
+
+    public int numberOfUnitSelected;
+    public int maxUnitToSelect = 6;
+
+    public List<NavMeshAgent> agents = new List<NavMeshAgent>();
+    //public List<Controller> ctrls = new List<Controller>();
+
+
+    public void addMe(NavMeshAgent agent)
+    {
+        agents.Add(agent);
+    }
+
+    public void removeMe(NavMeshAgent agent)
+    {
+        agents.Remove(agent);
+    }
+
+    public void removeAll()
+    {
+        agents.Clear();
+        numberOfUnitSelected = -1;
+    }
+
+    public void requestPath(Vector3 target)
+    {
+        if (numberOfUnitSelected == 5)
+        {
+            agents[0].SetDestination(target + new Vector3(0, 0, 0));
+            agents[1].SetDestination(target + new Vector3(1, 0, 0));
+            agents[2].SetDestination(target + new Vector3(-1, 0, 0));
+            agents[3].SetDestination(target + new Vector3(0, 0, -1));
+            agents[4].SetDestination(target + new Vector3(1, 0, -1));
+            agents[5].SetDestination(target + new Vector3(-1, 0, -1));
+        }
+        if (numberOfUnitSelected == 4)
+        {
+            agents[0].SetDestination(target + new Vector3(0, 0, 0));
+            agents[1].SetDestination(target + new Vector3(1, 0, 0));
+            agents[2].SetDestination(target + new Vector3(-1, 0, 0));
+            agents[3].SetDestination(target + new Vector3(0.5f, 0, -1));
+            agents[4].SetDestination(target + new Vector3(-0.5f, 0, -1));
+        }
+        if (numberOfUnitSelected == 3)
+        {
+            agents[0].SetDestination(target + new Vector3(0.5f, 0, 0));
+            agents[1].SetDestination(target + new Vector3(0.5f, 0, 0));
+            agents[2].SetDestination(target + new Vector3(-0.5f, 0, 0));
+            agents[3].SetDestination(target + new Vector3(0.5f, 0, -1));
+        }
+        if (numberOfUnitSelected == 2)
+        {
+            agents[0].SetDestination(target + new Vector3(0, 0, 0));
+            agents[1].SetDestination(target + new Vector3(1, 0, 0));
+            agents[2].SetDestination(target + new Vector3(-1, 0, 0));
+        }
+        if (numberOfUnitSelected == 1)
+        {
+            agents[0].SetDestination(target + new Vector3(0.5f, 0, 0));
+            agents[1].SetDestination(target + new Vector3(-0.5f, 0, 0));
+        }
+        if (numberOfUnitSelected == 0)
+        {
+            agents[0].SetDestination(target + new Vector3(0, 0, 0));
+        }
+    }
+}
---
+++ Assets/NPCController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCController : MonoBehaviour {
+
+    public MouvementManager mm;
+
+    public LayerMask walkable;
+    public LayerMask player;
+
+    Camera cam;
+    NavMeshAgent agent;
+
+    public bool isSelected;
+
+    //public GameObject selectedObject;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        mm = FindObjectOfType<MouvementManager>();
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isSelected)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if(Physics.Raycast(ray, out hit, 100,walkable))
+                {
+                    mm.requestPath(hit.point);
+                }
+            }
+        }
+	}
+
+    void Select()
+    {
+        isSelected = true;
+        mm.numberOfUnitSelected++;
+        mm.addMe(agent);
+    }
+
+    void UnSelect()
+    {
+        isSelected = false;
+        mm.numberOfUnitSelected--;
+        mm.removeMe(agent);
+    }
+
+    private void OnMouseDown()
+    {
+        if (isSelected)
+        {
+            UnSelect();
+        }
+        else if(mm.numberOfUnitSelected < mm.maxUnitToSelect  && !isSelected)
+        {
+            Select();
+        }
+    }
+}
